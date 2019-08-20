f6ba02f56bae360d668ef294ec6ec51db13ec122
Taylor Bennett <t4ybennett@gmail.com>
2019-08-20 14:34:47 +1200

    initialisation of project/assignment

---
+++ src/main/java/159251_Assignment1_James_Taylor/App.java
@@ -0,0 +1,13 @@
+package 159251_Assignment1_James_Taylor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
