2b8ad4509cfeb59d03be2e6e776a38a41ea42c69
Taylor Bennett <t4ybennett@gmail.com>
2019-08-20 17:01:24 +1200

    Created menubar items (File - New, Open and Save), they have no functionality yet..

--- Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
+++ Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
@@ -12,10 +12,33 @@ import java.awt.event.*;
 public class Assign1 extends JFrame implements ActionListener{
 
     public Assign1(){
+        // create the frame
         super("[Scribe]");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 600);
-		this.setVisible(true);
+
+        // This creates the main menu bar
+		JMenuBar menuBar = new JMenuBar();
+		this.setJMenuBar(menuBar);
+
+        // This creates a sub menu option "File" of the main menu bar 
+        JMenu fileOption = new JMenu("File");
+        menuBar.add(fileOption);
+
+        // this create the sub menu for the option "File" in the main menu bar
+        JMenu newOption = new JMenu("New");
+        JMenu openOption = new JMenu("Open");
+        JMenu saveOption = new JMenu("Save");
+        fileOption.add(newOption);
+        fileOption.add(openOption);
+        fileOption.add(saveOption);
+
+
+        // assign the menu bar to the window/frame
+        this.setJMenuBar(menuBar);
+
+        // Make the window/frame visible.
+        this.setVisible(true);
     }
     public void actionPerformed(ActionEvent event) {
 
