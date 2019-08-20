4dabd1ab0d46e8d14e4590701081b7d979c0af4a
James Waddell <jamesgwaddell@gmail.com>
2019-08-21 10:18:41 +1200

    Updates:
    -added some more JMenu buttons
    -added an action listener for the exit button
    -added an action event for when the exit button is clicked
    -made some variables private and defined them as part of the class

--- Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
+++ Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
@@ -10,7 +10,9 @@ import java.awt.event.*;
  *
  */
 public class Assign1 extends JFrame implements ActionListener{
-
+    private JMenuBar menuBar; 
+    private JMenu fileOption, searchOption, viewOption, manageOption, helpOption;
+    private JMenuItem newOption, saveOption, openOption, exitOption;
     public Assign1(){
         // create the frame
         super("[Scribe]");
@@ -18,23 +20,38 @@ public class Assign1 extends JFrame implements ActionListener{
         this.setSize(800, 600);
 
         // This creates the main menu bar
-		JMenuBar menuBar = new JMenuBar();
+		menuBar = new JMenuBar();
 		this.setJMenuBar(menuBar);
 
         // This creates a sub menu option "File" of the main menu bar 
-        JMenu fileOption = new JMenu("File");
+        fileOption = new JMenu("File");
+        searchOption = new JMenu("Search");
+        viewOption = new JMenu("View");
+        manageOption = new JMenu("Manage");
+        helpOption = new JMenu("Help");
         menuBar.add(fileOption);
+        menuBar.add(searchOption);
+        menuBar.add(viewOption);
+        menuBar.add(manageOption);
+        menuBar.add(helpOption);
+
 
         // this create the sub menu for the option "File" in the main menu bar
-        JMenu newOption = new JMenu("New");
-        JMenu openOption = new JMenu("Open");
-        JMenu saveOption = new JMenu("Save");
+        newOption = new JMenuItem("New");
+        openOption = new JMenuItem("Open");
+        saveOption = new JMenuItem("Save");
+        exitOption = new JMenuItem("Exit");
         fileOption.add(newOption);
         fileOption.add(openOption);
         fileOption.add(saveOption);
+        fileOption.add(exitOption);
+
 
         // assign the menu bar to the window/frame
         this.setJMenuBar(menuBar);
+
+        //Action listners
+        exitOption.addActionListener(this);
 
         // Create text area
         JTextArea textArea;
@@ -45,7 +62,10 @@ public class Assign1 extends JFrame implements ActionListener{
         this.setVisible(true);
     }
     public void actionPerformed(ActionEvent event) {
-
+        JComponent source = (JComponent) event.getSource();
+        if(source == exitOption){
+            System.exit(0);
+        }
     }
     public static void main( String[] args )
     {
