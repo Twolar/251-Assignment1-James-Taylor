fa94cbff95b787bc6e2f2d2d38c175b738fe688d
James Waddell <jamesgwaddell@gmail.com>
2019-08-20 15:45:47 +1200

    Notes:
    -Created a basic java JFrame with title in Assign1.java
    -Updated launch.json

--- Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
+++ Assignment1/src/main/java/Assignment1JamesTaylor/Assign1.java
@@ -1,13 +1,28 @@
 package Assignment1JamesTaylor;
 
+import java.awt.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.event.*;
+
 /**
  * Hello world!
  *
  */
-public class Assign1 
-{
+public class Assign1 extends JFrame implements ActionListener{
+
+    public Assign1(){
+        super("[Scribe]");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(800, 600);
+		this.setVisible(true);
+    }
+    public void actionPerformed(ActionEvent event) {
+
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Starting App..." );
+        new Assign1();
     }
 }
